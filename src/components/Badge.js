48aaa069ef5099967edfe3b8d0170cce2f1e6b12
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-12 23:19:23 -0500

    :construction: 11 - Styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import confLogo from '../images/badge-header.svg';
+import './styles/Badge.css';
 
 // We create a component
 class Badge extends React.Component {
   render() {
-    return <div>
-      <div>
-        <img src={confLogo} alt="Logo de la conferencia"></img>
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia"></img>
+        </div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="" alt="Avatar"></img>
+          <h1>Jozek<br />Hajduk</h1>
+        </div>
+        <div className="Badge__section-info">
+          <h3>Frontend engineer</h3>
+          <div>@HajdukSanchez</div>
+        </div>
+        <div className="Badge__footer">
+          #platziconf
+        </div>
       </div>
-      <div>
-        <img src="" alt="Avatar"></img>
-        <h1>Jozek<br />Hajduk</h1>
-      </div>
-      <div>
-        <p>Frontend engineer</p>
-        <p>@HajdukSanchez</p>
-      </div>
-      <div>
-        #platziconf
-      </div>
-    </div>;
+    );
   }
 }
 
