a9ec02ca13a2c8ba676fd076e9ef7935cd02f377
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-12 22:30:09 -0500

    :construction: 07 - JSX

--- src/index.js
+++ src/index.js
@@ -11,10 +11,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi badges</h1>; // We craete an element and store it into an variable
+// The first one is the type of element, the second are the properties and the third one are the elements inside
+const element1 = React.createElement(
+  'a',
+  {href: 'https://platzi.com'},
+  'Hola soy el children'
+  ); // Element with CreateElement
+const element = <h1>Hello, Platzi badges</h1>; // Element with JXS
 const container = document.getElementById('app');
 
 // ReactDOM.render(__QUE__, __DONDE__);
 // Que es el elemento, donde es el contenedor que lo va a poseer.
-ReactDOM.render(element, container);
+// ReactDOM.render(element, container);
+ReactDOM.render(element1, container);
 
+// const name = 'Jozek';
+// const jsx = <h1>Hola soy {name}</h1>
+// ReactDOM.render(jsx, container);
+
+const sum  = 3 + 3;
+const jsx = <h1>Hola soy {sum}</h1>
+ReactDOM.render(jsx, container);
